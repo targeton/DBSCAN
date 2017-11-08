e902fae04b286a6770a8d1df0c72d748b6aff092
zhoupeng <target_on@126.com>
2017-11-08 22:26:24 +0800

    initial DBSCAN

---
+++ DBSCAN/DBSCAN.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBSCAN
+{
+    public class DBSCAN
+    {
+        private List<ScanPoint> _allPoints;
+        public List<ScanPoint> AllPoints
+        {
+            get { return _allPoints ?? (_allPoints = new List<ScanPoint>()); }
+            set { _allPoints = value; }
+        }
+
+        private Func<ScanPoint, ScanPoint, double> _calculateDistance;
+
+        public DBSCAN(List<ScanPoint> allPoints, Func<ScanPoint, ScanPoint, double> calculateDistance)
+        {
+            _allPoints = allPoints;
+            _calculateDistance = calculateDistance;
+        }
+
+        public void ImplementAlgorithm(double radius, int minPts)
+        {
+            int clusterNum = 0;
+            foreach (var point in _allPoints)
+            {
+                if (point.VisitedType == VisitedType.Unvisited)
+                {
+                    point.VisitedType = VisitedType.Visited;
+                    List<ScanPoint> nearPoints = GetRegionPoints(point, radius);
+                    if (nearPoints.Count < minPts)
+                        point.PointType = PointType.Noise;
+                    else
+                        ExpandCluster(point, nearPoints, clusterNum++, radius, minPts);
+                }
+            }
+        }
+
+        private void ExpandCluster(ScanPoint corePoint, List<ScanPoint> nearPoint, int clusterNum, double radius, int minPts)
+        {
+            corePoint.PointType = PointType.Core;
+            corePoint.ClusterNum = clusterNum;
+            foreach (var p in nearPoint)
+            {
+                if (p.VisitedType == VisitedType.Unvisited)
+                {
+                    p.VisitedType = VisitedType.Visited;
+                    List<ScanPoint> neighborPoints = GetRegionPoints(p, radius);
+                    if (neighborPoints.Count >= minPts)
+                        ExpandCluster(p, neighborPoints, clusterNum, radius, minPts);
+                    else
+                    {
+                        p.PointType = PointType.Edge;
+                        p.ClusterNum = clusterNum;
+                    }
+                }
+                else
+                {
+                    if (p.ClusterNum < 0)
+                    {
+                        p.PointType = PointType.Edge;
+                        p.ClusterNum = clusterNum;
+                    }
+                }
+            }
+        }
+
+        private List<ScanPoint> GetRegionPoints(ScanPoint corePoint, double radius)
+        {
+            List<ScanPoint> result = new List<ScanPoint>();
+            List<ScanPoint> otherPoints = _allPoints.Except(new List<ScanPoint>() { corePoint }).ToList();
+            foreach (var p in otherPoints)
+            {
+                if (_calculateDistance(corePoint, p) <= radius)
+                    result.Add(p);
+            }
+            return result;
+        }
+    }
+
+    public class ScanPoint
+    {
+        private int _clusterNum = -1;
+        public int ClusterNum
+        {
+            get { return _clusterNum; }
+            set { _clusterNum = value; }
+        }
+
+        private PointType _pointType = PointType.None;
+        public PointType PointType
+        {
+            get { return _pointType; }
+            set { _pointType = value; }
+        }
+
+        private VisitedType _visitedType = VisitedType.Unvisited;
+        public VisitedType VisitedType
+        {
+            get { return _visitedType; }
+            set { _visitedType = value; }
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public object Tag { get; set; }
+    }
+
+    public enum PointType
+    {
+        None,
+        Noise,
+        Core,
+        Edge
+    }
+
+    public enum VisitedType
+    {
+        Unvisited,
+        Visited
+    }
+
+}
---
+++ DBSCAN/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBSCAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<ScanPoint, ScanPoint, double> calDistance = new Func<ScanPoint, ScanPoint, double>((p1, p2) =>
+            {
+                return Math.Sqrt(Math.Pow(p1.X - p2.X, 2.0) + Math.Pow(p1.Y - p2.Y, 2.0));
+            });
+            Random rand = new Random();
+            List<ScanPoint> points = new List<ScanPoint>();
+            int size = 15;
+            int pointNum = 30;
+            for (int i = 0; i < pointNum; )
+            {
+                int x = rand.Next(size);
+                int y = rand.Next(size);
+                if (points.FirstOrDefault(p => ((int)p.X == x) && ((int)p.Y == y)) == null)
+                {
+                    i++;
+                    points.Add(new ScanPoint() { X = x, Y = y });
+                }
+            }
+            Console.WriteLine(@"/******************Before DBSCAN(#:Point)********************/");
+            int[,] array = new int[size, size];
+            foreach (var p in points)
+            {
+                array[(int)p.X, (int)p.Y] = p.ClusterNum;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (array[i, j] == 0)
+                    {
+                        Console.Write('*');
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write('#');
+                        Console.Write(' ');
+                    }
+                }
+                Console.Write('\n');
+            }
+            Console.WriteLine(@"/*After DBSCAN(N:Noise,Upper Case:Core Point,Lower Case:Edge Point)*/");
+            double radius = 3.0;
+            int minPts = 3;
+            DBSCAN dbscan = new DBSCAN(points, calDistance);
+            dbscan.ImplementAlgorithm(radius, minPts);
+            foreach (var p in points)
+            {
+                if (p.PointType == PointType.Noise)
+                    array[(int)p.X, (int)p.Y] = -100;
+                else if (p.PointType == PointType.Core)
+                    array[(int)p.X, (int)p.Y] = p.ClusterNum + 1;
+                else
+                    array[(int)p.X, (int)p.Y] = (p.ClusterNum + 1) * 100;
+            }
+            char coreChar = 'A';
+            char edgeChar = 'a';
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (array[i, j] == 0)
+                    {
+                        Console.Write('*');
+                        Console.Write(' ');
+                    }
+                    else if (array[i, j] == -100)
+                    {
+                        Console.Write('N');
+                        Console.Write(' ');
+                    }
+                    else if (array[i, j] >= 100)
+                    {
+                        int edge = array[i, j] / 100 - 1;
+                        Console.Write(Convert.ToChar(edgeChar + edge));
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        int core = array[i, j] - 1;
+                        Console.Write(Convert.ToChar(coreChar + core));
+                        Console.Write(' ');
+                    }
+                }
+                Console.Write('\n');
+            }
+            Console.ReadLine();
+        }
+    }
+}
